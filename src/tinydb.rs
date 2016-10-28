2e1442cda37b88c1b74498b910cc4432919510b1
trent <tjmcc01@gmail.com>
2016-10-27 20:12:57 -0700

    changes to btree

--- src/tinydb.rs
+++ src/tinydb.rs
@@ -1,36 +1,21 @@
 use std::io;
 use std::io::prelude::*;
 use std::fs::File;
-use std::io::SeekFrom;
+use std::collections::BTreeMap;
 
-pub struct Db {
-    file: File
+pub struct Db<'a> {
+    file: File,
+    btree: BTreeMap<&'a [u8], &'a [u8]>,
 }
 
-impl Db {
-    pub fn open(path: &'static str) -> Result<Self, io::Error> { 
+impl<'a> Db<'a> {
+    pub fn open(path: &'static str) -> Result<Self, io::Error> {
         let file_ = try!(File::create(path));
 
-        return Ok (Db { file: file_ }); 
+        return Ok(Db { file: file_, btree: BTreeMap::new() });
     }
 
-    pub fn put(&mut self, key: &[u8], value: &[u8]) -> Result<usize, io::Error> {
-        let hash = djb2(key) % 1024;
-        let offset = hash % 1024;
-
-        try!(self.file.seek(SeekFrom::Start(offset)));
-        let bytes_written: usize = try!(self.file.write(value));
-        Ok(bytes_written)
+    pub fn put(&mut self, key: &'a [u8], value: &'a [u8]) -> Option<&[u8]> {
+        self.btree.insert(key, value)
     }
-
-}
-
-pub fn djb2(bytes: &[u8]) -> u64 {
-    let mut hash: u64 = 5381;
-    for b in bytes {
-        // hash * 33 + c
-        hash = (hash.wrapping_shr(5) + hash) + (*b as u64);
-    }
-
-    return hash;
 }
