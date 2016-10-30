b98d80677f5194e1dcad827a0f6d4fe2eb28f569
trent <tjmcc01@gmail.com>
2016-10-29 17:15:57 -0700

    Uses bincode to write to a db

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,23 @@ mod tests {
     use tinydb;
 
     #[test]
-    fn test_open() {
+    fn test_open_ok() {
         let d = tinydb::Db::open("test.db");
         assert_eq!(d.is_ok(), true);
     }
 
     #[test]
-    fn test_put() {
+    fn test_put_ok() {
         let mut d = tinydb::Db::open("test.db").unwrap();
-        let w = d.put("a".as_bytes(), "b".as_bytes()).unwrap();
-        assert_eq!(w, 1);
+        let w = d.put("a".as_bytes(), "b".as_bytes());
+        assert_eq!(w.is_ok(), true);
+    }
+
+    #[test]
+    fn test_put_and_get(){
+        let mut d = tinydb::Db::open("test1.db").unwrap();
+        let w = d.put("a".as_bytes(), "b".as_bytes());
+        let res = d.get("a".as_bytes()).unwrap();
+        assert_eq!(res.is_some(), true);
     }
 }
--- src/tinydb.rs
+++ src/tinydb.rs
@@ -1,21 +1,41 @@
 use std::io;
 use std::io::prelude::*;
+extern crate bincode;
+
 use std::fs::File;
 use std::collections::BTreeMap;
 
-pub struct Db<'a> {
+
+pub struct Db {
     file: File,
-    btree: BTreeMap<&'a [u8], &'a [u8]>,
+    btree: BTreeMap<Vec<u8>, Vec<u8>>,
 }
 
-impl<'a> Db<'a> {
+impl Db {
     pub fn open(path: &'static str) -> Result<Self, io::Error> {
         let file_ = try!(File::create(path));
-
-        return Ok(Db { file: file_, btree: BTreeMap::new() });
+        
+        Ok(Db {
+            file: file_,
+            btree: BTreeMap::new(),
+        })
     }
 
-    pub fn put(&mut self, key: &'a [u8], value: &'a [u8]) -> Option<&[u8]> {
-        self.btree.insert(key, value)
+    pub fn put(&mut self, key: &[u8], value: &[u8]) -> Result<usize, io::Error> {
+        self.btree.insert(key.to_vec(), value.to_vec());
+        let limit = bincode::SizeLimit::Bounded(1000000);
+        let s: Vec<u8> = bincode::serde::serialize(&self.btree, limit).unwrap();
+        return self.file.write(&s);
+    }
+
+    pub fn get(&mut self, key: &[u8]) -> Result<Option<Vec<u8>>, io::Error> {
+        let mut buf = Vec::new();
+        try!(self.file.read_to_end(&mut buf));
+        let map: BTreeMap<Vec<u8>, Vec<u8>> = bincode::serde::deserialize(&buf).unwrap();
+        let rtn: Option<&Vec<u8>> = map.get(&key.to_vec());
+        match rtn {
+            None => return Ok(None),
+            Some(x) => return Ok(Some(x.to_owned())),
+        }
     }
 }
