c65c945d1974f63e8348e0b3aad3f631f9ebdabb
trent <tjmcc01@gmail.com>
2016-11-19 00:09:04 +0100

    Finishes test

--- src/serialize.rs
+++ src/serialize.rs
@@ -1,11 +1,11 @@
 use std::collections::BTreeMap;
 
 
-trait CanSerialiaze {
+trait CanSerialize {
     fn serialize(&self) -> Vec<u8>;
 }
 
-impl<'a> CanSerialiaze for BTreeMap<&'a [u8], &'a [u8]> {
+impl<'a> CanSerialize for BTreeMap<&'a [u8], &'a [u8]> {
 	fn serialize(&self) -> Vec<u8> {
 		let mut v: Vec<u8> = Vec::new();
 		for (key, value) in self.iter() {
@@ -19,9 +19,17 @@ impl<'a> CanSerialiaze for BTreeMap<&'a [u8], &'a [u8]> {
 
 #[cfg(test)]
 mod tests {
+	use super::CanSerialize;
+	use std::collections::BTreeMap;
 
 	#[test]
-    fn test_open_ok() {
-        
+    fn test1_ok() {
+        let mut t = BTreeMap::new();
+        t.insert("a".as_bytes(), "b".as_bytes());
+        assert_eq!(t.serialize()[0], "a".as_bytes().to_vec()[0]);
+        assert_eq!(t.serialize()[1], "b".as_bytes().to_vec()[0]);
+
+        let v = "ab".as_bytes().to_vec();
+        assert_eq!(t.serialize()[0], v[0]);
     }
 }
